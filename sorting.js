e2de96ccd4eb05a2b1a9cb8b5babff9f6ac8d9ce
Sin Cheung Ko <scko823@gmail.com>
2016-09-03 15:58:00 -0700

    bubble sort

--- sorting.js
+++ sorting.js
@@ -18,5 +18,16 @@ function mergeSort (array){
 	}
 }
 
-
-
+//bubble sort
+function bubbleSort(array){
+	for (let i=array.length;i>=0;i--){
+		for (let j=0; j<i-1; j++){
+			if (array[j]>array[j+1]){
+				let temp = array[j]
+				array[j] = array[j+1]
+				array[j+1] = temp
+			}
+		}
+	}
+	return array
+}
